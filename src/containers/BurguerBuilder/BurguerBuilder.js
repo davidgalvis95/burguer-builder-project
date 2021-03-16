b7ebad97101777dfbc8868724e23ace56d990c6f
David Galvis <ivan.galvis@project44.com>
2021-03-15 22:53:36 -0500

    section 15. enabling the order now button

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -26,8 +26,6 @@ class BurguerBuilder extends Component {
     // }
 
     state = {
-        totalPrice: 4,
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: null
@@ -52,7 +50,7 @@ class BurguerBuilder extends Component {
         }).reduce((sum, el) => {
             return sum + el;
         }, 0);
-        this.setState({purchasable: sum > 0})
+        return sum > 0;
     }
 
     addIngredientHandler = (type) => {
@@ -143,7 +141,7 @@ class BurguerBuilder extends Component {
                         ingredientRemoved={this.props.onIngredientDeleted}
                         //here we pass the entire object
                         disabled={disabledInfo}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
                         price={this.props.price}
                     />
