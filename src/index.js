bd372e56678da0d7363fdbc023d6a1d87f38c2f8
David Galvis <ivan.galvis@project44.com>
2021-03-20 13:37:20 -0500

    section 17 enabling redux-thunk and middlewares to execute async code

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
 import burguerBuilder from './store/reducers/burguerBuilder';
 
 //here we need the basic setup because
-const store = createStore(burguerBuilder, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(burguerBuilder, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
