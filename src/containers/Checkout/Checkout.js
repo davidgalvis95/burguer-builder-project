3ba19285a4207cee6be50d54ce8bb9a021a5d646
David Galvis <ivan.galvis@project44.com>
2021-03-23 22:44:06 -0500

    section 17. fixing undefined when loading the ingredients from redux in Checkout component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {Route} from 'react-router-dom';
-import { connect } from 'react-redux';
+import {Route, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from "./ContactData/ContactData";
@@ -17,20 +17,22 @@ class Checkout extends Component {
 
     render() {
         //{/*So far the ingredients we will pass here will be dummy ones*/}
-        return(
-            <div>
+        let summary = <Redirect to="/"/>
+        if (this.props.ings) {
+            summary = (<div>
                 <CheckoutSummary
                     ingredients={this.props.ings}
-                    checkoutCancelled = {this.checkoutCancelledHandler}
-                    checkoutContinued = {this.checkoutContinuedHandler}
+                    checkoutCancelled={this.checkoutCancelledHandler}
+                    checkoutContinued={this.checkoutContinuedHandler}
                 />
                 {/*The render is no longer needed due that ContactData will no longer pass the props from this component to the ContactData,
                 ContactData will query them from Redux*/}
                 <Route path={this.props.match.path + '/contact-data'}
                        component={ContactData}/>
 
-            </div>
-        )
+            </div>)
+        }
+        return summary;
     }
 }
 
@@ -41,4 +43,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps) (Checkout);
+export default connect(mapStateToProps)(Checkout);
