9e592d772f6363b6051827ad512ff1c3543b32af
David Galvis <ivan.galvis@project44.com>
2021-03-29 19:56:05 -0500

    section 18. fixing a small bug in reducer

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -8,7 +8,7 @@ const initialState = {
     loading: false
 }
 
-const authStart = (state = initialState, action) => {
+const authStart = (state, action) => {
     return updateObject(state, {error: null, loading: true})
 }
 
@@ -28,7 +28,7 @@ const authFail = (state, action) => {
     })
 }
 
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.AUTH_START: return authStart(state, action);
         case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
