9fc9447ec0b0959d28d9291e7746d17ef3eafa2e
David Galvis <ivan.galvis@project44.com>
2021-04-05 17:16:03 -0500

    section 18. guarding routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Route, Switch, withRouter} from "react-router";
+import {Redirect, Route, Switch, withRouter} from "react-router";
 import {connect} from 'react-redux';
 
 import Layout from './hoc/Layout/Layout'
@@ -12,17 +12,6 @@ import * as actions from './store/actions/index'
 
 class App extends Component {
 
-  //this is done to test that when the component gets unmounted the ejected axios interceptors get called  in the wrapper component
-  // state = {
-  //     show: true
-  // }
-  //
-  // componentDidMount() {
-  //     setTimeout(()=>{
-  //         this.setState({show:false})
-  //     },5000)
-  // }
-
     //This call on the did mount will ensure that every subcomponent(this encloses all the app) from which this app component is parent of,
     //everytime is updated or re-rendered will check if is time to log out due to expiration of the token
     componentDidMount() {
@@ -30,22 +19,36 @@ class App extends Component {
     }
 
     render () {
-      // const burguerBuilder = this.state.show? <BurguerBuilder></BurguerBuilder> :null;
-    return (
-        <div>
-          <Layout>
-            <Switch>
-                {/*The order with the switch needs to be from the most specific to the less specific one*/}
+
+        let theRoutes = (<Switch>
+                <Route path="/" component={BurguerBuilder}/>
+                <Route path="/auth" component={Auth}/>
+                <Redirect to="/"/>
+            </Switch>);
+
+        if(this.props.isAuthenticated){
+            theRoutes = (<Switch>
                 <Route path="/checkout" component={Checkout}/>
                 <Route path="/orders" component={Orders}/>
-                <Route path="/auth" component={Auth}/>
                 <Route path="/logout" component={Logout}/>
                 <Route path="/" component={BurguerBuilder}/>
-            </Switch>
-          </Layout>
-        </div>
-    );
+            </Switch>);
+        }
+
+        return (
+            <div>
+              <Layout>
+                  {theRoutes}
+              </Layout>
+            </div>
+        );
   };
+}
+
+const mapStateToProps = state =>{
+    return{
+        isAuthenticated: state.auth.token !== null
+    }
 }
 
 const mapDispatchToProps = dispatch => {
