949c3dff20f125067bd76b7aae47e75d43b4f22e
David Galvis <ivan.galvis@project44.com>
2021-04-08 20:12:38 -0500

    section 19. sharing the validation method

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,7 +8,7 @@ import Spinner from '../../../components/UI/Spinner/Spinner'
 import Input from '../../../components/UI/Input/Input'
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
-import {updateObject} from "../../../shared/utility";
+import {updateObject, checkValidity} from "../../../shared/utility";
 
 
 class ContactData extends Component {
@@ -118,35 +118,6 @@ class ContactData extends Component {
         formIsValid: false,
     }
 
-    checkValidity(value, rules) {
-
-        let isValid = true;
-
-        //This is the alternative way of solving the: cannot read property 'X' of undefined
-        // if(!rules){
-        //     return true;
-        // }
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-
-        if(rules.isEmail){
-            const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            isValid = emailRegex.test(String(value).toLowerCase());
-
-        }
-
-        return isValid;
-    }
-
     orderHandler = (event) => {
         event.preventDefault();
         // console.log(this.props.ingredients);
@@ -183,7 +154,7 @@ class ContactData extends Component {
         //reusing the updatedObject function here too, as in the actions of the redux state
         const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
             value: event.target.value,
-            valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+            valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
             touched: true
         })
         // console.log(event.target.value);
